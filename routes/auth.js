377448c012297cc3a86cca3218b4c3f75d9e9b2d
yusufhalby <yosufkhaled@eng.aswu.edu.eg>
2023-07-10 13:50:18 +0300

    trimming password and emai, normalize email

--- routes/auth.js
+++ routes/auth.js
@@ -22,29 +22,30 @@ const router = express.Router();
 // /auth/signup => PUT
 router.put('/signup', [
         body('email')
-            .isEmail()
-            .withMessage('Please enter a valid email.')
-            .custom((value, {req}) => {
-                return User.findOne({email: value})
-                    .then(userDoc => {
-                        if (userDoc) {
-                            return Promise.reject('Email already in use');
-                        }
-                    })
+        .trim()
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, {req}) => {
+            return User.findOne({email: value})
+                .then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject('Email already in use');
+                    }
+                })
         })
         .normalizeEmail(),
         body('password')
-            .trim()
-            .isLength({min: 5}), //minimum length
+        .trim()
+        .isLength({min: 5}), //minimum length
         body('name')
-            .trim()
-            .not()
-            .isEmpty()
+        .trim()
+        .not()
+        .isEmpty()
 
     ],
     authController.signup);
 
 // auth/login => POST
-router.post('/login', authController.login);
+router.post('/login', [body('email').normalizeEmail().trim(), body('password').trim()], authController.login);
 
 module.exports = router;
